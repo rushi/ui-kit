f05a7d2d034e0349a1d7976c42049ae817df686f
Rushi Vishavadia <rushi@xola.com>
2022-03-06 20:14:22 +0530

    Remove deprecated arg from package.json and put in main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,10 @@
 module.exports = {
     stories: ["../src/**/*.stories.@(js|jsx|mdx)"],
-
+    framework: "@storybook/react",
+    staticDirs: ["../public"],
     core: {
         builder: "webpack5",
     },
-
     addons: [
         "@storybook/addon-postcss",
         "@storybook/addon-links",
